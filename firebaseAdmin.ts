933a0a02cd7b95cd36d025330b7a68d96fda87b2
Palladiusz <palladiusz.ra@gmail.com>
2023-01-17 20:54:02 +0100

    dot env

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -9,9 +9,9 @@ import serviceAccount from "./secret.json";
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.REACT_APP_PRIVATE_KEY,
+      clientEmail: process.env.REACT_APP_CLIENT_EMAIL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
     }),
     databaseURL: "https://carleon-guide.firebaseio.com",
   });
